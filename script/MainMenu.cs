22a2636954944490ddfacb24248cbd63f7d1f9f4
sebasion9 <kansebakk@gmail.com>
2025-06-19 21:40:22 +0200

    edit main menu style add submenu for abilities add submenu for attributes

--- script/MainMenu.cs
+++ script/MainMenu.cs
@@ -1,11 +1,39 @@
 using Godot;
 using System;
 
+enum SubMenuView
+{
+    Abilities = 1,
+    Attributes= 2,
+};
 public partial class MainMenu : Control
 {
+
+    private Control attrMenu;
+    private Control abilityMenu;
+
+    public override void _Ready()
+    {
+        this.abilityMenu = GetNode<Control>("SubmenuContainer/AbilitiesMenu");
+        this.attrMenu = GetNode<Control>("SubmenuContainer/AttrMenu");
+        this.abilityMenu.Hide();
+        this.attrMenu.Show();
+    }
 
     public void OnStartClick()
     {
         GetTree().ChangeSceneToFile("res://script/Arena.tscn");
+    }
+
+    public void OnAbilitiesClick()
+    {
+        this.abilityMenu.Show();
+        this.attrMenu.Hide();
+    }
+
+    public void OnAttributesClick()
+    {
+        this.abilityMenu.Hide();
+        this.attrMenu.Show();
     }
 }
