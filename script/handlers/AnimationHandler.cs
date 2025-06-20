e94fe7dee1b036574c7ebe44c3b9924edca7f9e9
sebasion9 <kansebakk@gmail.com>
2025-06-20 18:50:40 +0200

    add jump and fall animations

--- script/handlers/AnimationHandler.cs
+++ script/handlers/AnimationHandler.cs
@@ -14,18 +14,36 @@ public partial class AnimationHandler : Node
         
         Sprite.FlipH = direction < 0;
     }
-    public void HandleWalk(float speed)
+
+    public void HandleAnimations(CharacterBody2D body, float speed)
     {
+
         var animSpeed = speed / 200;
-        var walk = Input.IsActionPressed("move_right") || Input.IsActionPressed("move_left");
-        if (walk)
+        var isGoingUp = body.Velocity.Normalized().Y < 0;
+
+
+        if (body.IsOnFloor())
         {
-            Sprite.Play("walk", animSpeed);
-        }
-        else
-        {
+            if (body.Velocity.Length() > 0)
+            {
+                Sprite.Play("walk", animSpeed);
+                return;
+            }
+
             Sprite.Play("idle");
+            return;
+
         }
 
+        if (isGoingUp)
+        {
+            Sprite.Play("jump");
+            return;
+
+        }
+        Sprite.Play("fall");
+        
+
     }
+
 }
